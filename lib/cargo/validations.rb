5f7565faee33fb0c98f121f08cd878c33e844028
Sam Kirchmeier <sam.kirchmeier@gmail.com>
2008-08-08 12:49:24 -0400

    Alphabetized validations

--- lib/cargo/validations.rb
+++ lib/cargo/validations.rb
@@ -1,5 +1,41 @@
 module Cargo
   module Validations
+    # Validates the existence of the associated file on disk
+    #
+    # ==== Example
+    #
+    #   class Image < ActiveRecord::Base
+    #     cargo :image
+    #
+    #     validates_file_exists :image, :on => :create
+    #   end
+    #
+    # ==== Parameters
+    #
+    # [<tt>name</tt>]
+    #   The name of the <tt>cargo</tt> association
+    # [<tt>options</tt>]
+    #   A hash of options for the validation
+    #
+    # ==== Options
+    #
+    # [<tt>:message</tt>]
+    #   A custom error message
+    # [<tt>:on</tt>]
+    #   Specifies when this validation is active (default is :save, other
+    #   options :create, :update)
+    #
+    def validates_file_exists(name, options = {})
+      options[:message] ||= 'must be set'
+      send(validation_method(options[:on] || :save), options) do |record|
+        file = record.send(name)
+        record.errors.add(name, options[:message]) if
+          !file.is_a?(CargoFile) ||
+          (file.new_record? && !file.new_data?) ||
+          (!file.new_record? && !File.exist?(file.absolute_filename))
+      end
+    end
+
     # Validates the extension of the specified file
     #
     # ==== Example
@@ -41,42 +77,6 @@ module Cargo
             record.errors.add(name, options[:message])
           end
         end
-      end
-    end
-
-    # Validates the existence of the associated file on disk
-    #
-    # ==== Example
-    #
-    #   class Image < ActiveRecord::Base
-    #     cargo :image
-    #
-    #     validates_file_exists :image, :on => :create
-    #   end
-    #
-    # ==== Parameters
-    #
-    # [<tt>name</tt>]
-    #   The name of the <tt>cargo</tt> association
-    # [<tt>options</tt>]
-    #   A hash of options for the validation
-    #
-    # ==== Options
-    #
-    # [<tt>:message</tt>]
-    #   A custom error message
-    # [<tt>:on</tt>]
-    #   Specifies when this validation is active (default is :save, other
-    #   options :create, :update)
-    #
-    def validates_file_exists(name, options = {})
-      options[:message] ||= 'must be set'
-      send(validation_method(options[:on] || :save), options) do |record|
-        file = record.send(name)
-        record.errors.add(name, options[:message]) if
-          !file.is_a?(CargoFile) ||
-          (file.new_record? && !file.new_data?) ||
-          (!file.new_record? && !File.exist?(file.absolute_filename))
       end
     end
   end
